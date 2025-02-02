65a363b36f5bd97b49a128d519166721bd628cfd
Shashank Jaiswal <jaisw7@gmail.com>
2025-02-02 09:02:36 -0500

    Reduce the width of Kn so that it does not advect far

--- fast_spectral/mixing_regime/plots/Kn.py
+++ fast_spectral/mixing_regime/plots/Kn.py
@@ -7,18 +7,18 @@ outfile = "Kn.pdf"
 
 
 # Define the grid
-x = np.linspace(0, 2, 50)
-y = np.linspace(0, 2, 50)
+x = np.linspace(0, 2, 100)
+y = np.linspace(0, 2, 100)
 x, y = np.meshgrid(x, y)
-z = (1e-4 + 0.5 * (np.tanh(7 - 5 * x) + np.tanh(-3 + 5 * x))) * (
-    1e-4 + 0.5 * (np.tanh(7 - 5 * y) + np.tanh(-3 + 5 * y))
+z = (1e-4 + 0.5 * (np.tanh(6 - 5 * x) + np.tanh(-4 + 5 * x))) * (
+    1e-4 + 0.5 * (np.tanh(6 - 5 * y) + np.tanh(-4 + 5 * y))
 )
 
 cp = ContourPlot()
 fig, ax = cp.subplots()
-contour = ax.contourf(x, y, z, levels=np.linspace(0, 1, 9), cmap=cp.cmap)
+contour = ax.contourf(x, y, z, levels=np.linspace(0, 0.6, 13), cmap=cp.cmap)
 ax.set(xlabel="X (mm)", ylabel="Y (mm)")
 ax.set(xlim=(0, 2), ylim=(0, 2))
-cbar = cp.colorbar(contour, "Kn:", ticks=np.linspace(0, 1, 5))
+cbar = cp.colorbar(contour, "Kn:", ticks=np.linspace(0, 0.6, 7))
 cp.savepdf(outfile)
 subprocess.call(["pdfcrop", outfile, outfile])

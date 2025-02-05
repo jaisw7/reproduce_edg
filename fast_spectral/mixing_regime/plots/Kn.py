fe57f9855adedd0fecbdf5ea2e2f43dfdeb3f58d
Shashank Jaiswal <jaisw7@gmail.com>
2025-02-05 03:57:08 -0500

    Move from linear to logarithmic plot

--- fast_spectral/mixing_regime/plots/Kn.py
+++ fast_spectral/mixing_regime/plots/Kn.py
@@ -2,23 +2,34 @@ import subprocess
 
 import numpy as np
 from edgfs2D.post_process.contour_plot import ContourPlot
+from matplotlib.colors import LogNorm
+from matplotlib.ticker import FuncFormatter
 
-outfile = "Kn.pdf"
+
+def exp_format(x, pos):
+    return "$10^{{{0}}}$".format(int(f"{x:.1e}".split("e")[1]))
 
 
-# Define the grid
-x = np.linspace(0, 2, 100)
-y = np.linspace(0, 2, 100)
-x, y = np.meshgrid(x, y)
-z = (1e-4 + 0.5 * (np.tanh(6 - 5 * x) + np.tanh(-4 + 5 * x))) * (
-    1e-4 + 0.5 * (np.tanh(6 - 5 * y) + np.tanh(-4 + 5 * y))
-)
+if __name__ == "__main__":
+    outfile = "Kn.pdf"
 
-cp = ContourPlot()
-fig, ax = cp.subplots()
-contour = ax.contourf(x, y, z, levels=np.linspace(0, 0.6, 13), cmap=cp.cmap)
-ax.set(xlabel="X (mm)", ylabel="Y (mm)")
-ax.set(xlim=(0, 2), ylim=(0, 2))
-cbar = cp.colorbar(contour, "Kn:", ticks=np.linspace(0, 0.6, 7))
-cp.savepdf(outfile)
-subprocess.call(["pdfcrop", outfile, outfile])
+    # Define the grid
+    x = np.linspace(0, 2, 100)
+    y = np.linspace(0, 2, 100)
+    x, y = np.meshgrid(x, y)
+    z = (1e-4 + 0.5 * (np.tanh(6 - 5 * x) + np.tanh(-4 + 5 * x))) * (
+        1e-4 + 0.5 * (np.tanh(6 - 5 * y) + np.tanh(-4 + 5 * y))
+    )
+
+    cp = ContourPlot()
+    fig, ax = cp.subplots()
+    levels = np.logspace(-7, 0, 8)
+    contour = ax.contourf(x, y, z, levels=levels, cmap=cp.cmap, norm=LogNorm())
+    ax.set(xlabel="X (mm)", ylabel="Y (mm)")
+    ax.set(xlim=(0, 2), ylim=(0, 2))
+    cbar = cp.colorbar(
+        contour, "Kn:", ticks=levels, inset_params=[0.38, 1.05, 0.6, 0.05]
+    )
+    cbar.ax.xaxis.set_major_formatter(FuncFormatter(exp_format))
+    cp.savepdf(outfile)
+    subprocess.call(["pdfcrop", outfile, outfile])
